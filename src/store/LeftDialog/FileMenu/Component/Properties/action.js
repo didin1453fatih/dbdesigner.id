b9fb820362161f24b0e43e21eece74689aa4183e
Didin1453fatih <didin1453fatih@gmail.com>
2019-08-21 06:14:07 +0700

    update properties state

---
+++ src/store/LeftDialog/FileMenu/Component/Properties/action.js
@@ -0,0 +1,51 @@
+import { message } from "ant-design-vue";
+import UpdateProject from "@/request/updateProject.request";
+import DeleteProject from "@/request/deleteProject.request";
+import { request2, requestHelper } from "@/helper/RequestConnector";
+export default {
+  updateProject: request2(async context => {
+    context.commit("setLoding", true);
+    try {
+      await requestHelper(UpdateProject, {
+        id: context.rootState.diagram.projectDescription.id,
+        title: context.state.title,
+        description: context.state.description
+      });
+      message.success("Updated", 2);
+      context.rootDispatch("diagram/loadProject", {
+        id: context.rootState.diagram.projectDescription.id
+      });
+      context.rootCommit("LeftDialog/FileMenu/Layout/setVisible", false);
+    } catch (error) {
+      if (error.code === 10) {
+        message.error("Login first to update this project", 2);
+        context.rootCommit("LeftDialog/FileMenu/Layout/setVisible", true);
+        context.rootCommit("LeftDialog/FileMenu/Layout", "login");
+      } else {
+        message.error(error.message, 2);
+      }
+    }
+    context.commit("setLoding", false);
+  }),
+  deleteProject: request2(async context => {
+    context.rootCommit("GlobalLoading/setVisible", true);
+    try {
+      await requestHelper(DeleteProject, {
+        id: context.rootState.diagram.projectDescription.id
+      });
+      message.success("Deleted", 2);
+      context.commit("setTitle", null);
+      context.commit("setDescription", null);
+      context.rootCommit("diagram/deletedData");
+    } catch (error) {
+      if (error.code === 10) {
+        message.error("Login first to delete this project", 2);
+        context.rootCommit("LeftPanel/setVisible", true);
+        context.rootCommit("LeftPanel/setPanelName", "login");
+      } else {
+        message.error(error.message, 2);
+      }
+    }
+    context.rootCommit("GlobalLoading/setVisible", false);
+  })
+};
--- src/store/LeftDialog/FileMenu/Component/module.js
+++ src/store/LeftDialog/FileMenu/Component/module.js
@@ -2,12 +2,14 @@ import NewProject from "./NewProject/module";
 import Open from "./Open/module";
 import Account from "./Account/module";
 import Login from "./Login/module";
+import Properties from "./Properties/module";
 export default {
   namespaced: true,
   modules: {
     Account,
     NewProject,
     Open,
-    Login
+    Login,
+    Properties
   }
 };
